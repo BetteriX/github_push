549867f4016230dad024be150285f9adc03b7262
BetteriX <szabozsolt430@gmail.com>
2024-04-25 15:22:07 +0200

    test

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ char* get_git_remote_url() {
     while (fgets(buffer, MAX_SIZE, fp) != NULL) {
         if (strstr(buffer, "remote =") != NULL) {
             // Find the URL part and copy it into a new string
-            char *url_start = strstr(buffer, "remote =") /*+ strlen("remote =h")*/;
+            char *url_start = strstr(buffer, "remote =") + strlen("remote =h");
             url = strdup(url_start);
             // Remove trailing newline characters
             url[strcspn(url, "\r\n")] = '\0';
